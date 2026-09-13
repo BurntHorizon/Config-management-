167702726ae510cfe16862668426f64df42fbc86
BurntHorizon <98994028+BurntHorizon@users.noreply.github.com>
2026-09-13 14:54:58 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+required_providers {
+docker = {
+source = "kreuzwerker/docker"
+version = "~> 3.6"
+}
+}
+}
+provider "docker" {}
+resource "docker_image" "nginx" {
+name = "nginx:latest"
+keep_locally = false
+}
+resource "docker_container" "nginx" {
+name = "tf-demo-nginx"
+image = docker_image.nginx.image_id
+ports {
+internal = 80
+external = 8080
+}
+}
